48c2e5fee96a4cead2ff034e4e166423e01deb45
pduren <57735971+kParkz@users.noreply.github.com>
2023-12-09 11:12:21 -0600

    floors n' doors

--- Assets/Scripts/MorePlayerFunc.cs
+++ Assets/Scripts/MorePlayerFunc.cs
@@ -8,17 +8,31 @@ public class MorePlayerFunc : MonoBehaviour
     public GameObject endPoint;
     Vector3 endPointInitPos;
     float distanceFromEndPoint;
+    private bool isLevelComplete = false;
+    public GameObject rightShoji;
+    public GameObject rightShojiEndPoint;
+    public GameObject leftShoji;
+    public GameObject leftShojiEndPoint;
+    Vector3 rightShojiInitPos;
+    Vector3 leftShojiInitPos;
 
     void Start()
     {
         endPointInitPos = endPoint.transform.position;
+        rightShojiInitPos = rightShoji.transform.position;
+        leftShojiInitPos = leftShoji.transform.position;
     }
 
     void Update()
     {
         distanceFromEndPoint = Vector3.Distance(player.transform.position, endPointInitPos);
         if (distanceFromEndPoint < 3) {
-            Debug.Log("end level");
+            isLevelComplete = true;
+        }
+
+        if (isLevelComplete) {
+            rightShoji.transform.position = Vector3.MoveTowards(rightShojiInitPos, rightShojiEndPoint.transform.position, 1f);
+            leftShoji.transform.position = Vector3.MoveTowards(leftShojiInitPos, leftShojiEndPoint.transform.position, 1f);
         }
     }
 }
