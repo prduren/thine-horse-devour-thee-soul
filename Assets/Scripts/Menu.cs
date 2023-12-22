f1d24ae4ef2af0d5023fb509b2ab11f72a27854b
pduren <57735971+kParkz@users.noreply.github.com>
2023-12-22 12:04:32 -0600

    options init, color change toggle, other menu btns

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
+
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private PixelStylizerCamera pixelStylizerCamera;
+    public static bool FullScreenMode = true;
+
     public void StartGame() {
         SceneManager.LoadScene("Intro");
     }
+
+    public void Options() {
+        // enable options buttons
+        // also disable options buttons
+    }
+
+    public void Quit () {
+        Application.Quit();
+    }
+
+    public void ToggleFullScreen() {
+        if (Screen.fullScreen) {
+            FullScreenMode = false;
+            Screen.fullScreen = false;
+        } else if (!Screen.fullScreen) {
+            FullScreenMode = true;
+            Screen.fullScreen = true;
+        }   
+    }
+
+    public void ToggleCameraColor() {
+        System.Random random = new System.Random();
+        pixelStylizerCamera.SetPreset(random.Next(1, 9));
+    }
+
 }
